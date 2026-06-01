0c3286bd0b726775c9cdd5ae43c47748338958a4
kimsehee <80444956+kimsehee-8135@users.noreply.github.com>
2026-06-01 11:24:09 +0900

    fix: 에러메시지 띄어쓰기 (#58)

--- src/main/java/com/oit/dondok/infra/image/exception/ImageErrorCode.java
+++ src/main/java/com/oit/dondok/infra/image/exception/ImageErrorCode.java
@@ -8,7 +8,7 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum ImageErrorCode implements ErrorCode {
-  IMAGE_READ_FAILED(HttpStatus.UNPROCESSABLE_ENTITY, "이미지를 읽을 수없습니다."),
+  IMAGE_READ_FAILED(HttpStatus.UNPROCESSABLE_ENTITY, "이미지를 읽을 수 없습니다."),
   IMAGE_ENCODE_FAILED(HttpStatus.UNPROCESSABLE_ENTITY, "이미지 재인코딩에 실패했습니다."),
   IMAGE_NOT_FOUND(HttpStatus.NOT_FOUND, "이미지를 찾을 수 없습니다.");
 
